2f4068d9e595634461bc73d15537d41df361a997
Cong Hao <77195793+hori2012@users.noreply.github.com>
2022-10-21 06:27:58 +0700

    Add files via upload

---
+++ ConnectionStringSql.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNhanVien
+{
+    internal class ConnectionStringSql
+    {
+        static public void connection(ref SqlConnection cnn)
+        {
+            string connectionString = @"Data Source = LAPTOP-7ABHDLJ3\SQLEXPRESS; Initial Catalog = QLNhanVien; User ID = CongHao; Password = conghao20";
+            cnn = new SqlConnection(connectionString);
+        }
+
+    }
+}
